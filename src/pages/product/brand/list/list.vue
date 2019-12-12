8a7895f3a18d08449872c4735ee2b12b5ed816e6
qinjianfei <qc_406eea3bca2c47ceac78d156a1b94982@git.code.tencent.com>
2019-12-12 12:08:08 +0800

    完善 品牌列表管理

--- src/pages/product/brand/list/list.vue
+++ src/pages/product/brand/list/list.vue
@@ -1,14 +1,100 @@
 <template>
   <div>
-    品牌列表
+    <Table border :columns="columns" :data="data">
+      <template slot-scope="{row, index}" slot="action">
+        <Button type="primary" size="small" style="margin-right: 5px" @click="handleBrandEdit(row, index)">编辑</Button>
+        <Button v-if="row.brand_status ? false : true" type="error" size="small" @click="handleBrandRemove(row)">删除</Button>
+      </template>
+      <template slot-scope="{row, index}" slot="logo">
+        <div v-viewer>
+          <img width="100" height="100" :src="row.brand_logo" alt="">
+        </div>
+        
+      </template>
+      <template slot-scope="{row, index}" slot="status">
+        <i-switch 
+        disabled
+        :value="row.brand_status ? false : true"
+        size="large"
+        true-color="#13ce66" 
+        false-color="#ff4949">
+          <span slot="open">显示</span>
+          <span slot="close">隐藏</span>
+        </i-switch>
+      </template>
+    </Table>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import 'viewerjs/dist/viewer.css'
+import Viewer from 'v-viewer'
+
+Vue.use(Viewer)
+import {getBrand, removeBrand} from 'service/brand-service'
+import mixin from 'utils/mixins'
 export default {
   name: 'product_brand_list',
+  mixins: [mixin],
   data() {
     return {
+      switchShow: true,
+      data: [],
+      columns: [
+        {
+          key: 'id',
+          title: 'ID'
+        },
+        {
+          key: 'brand_name',
+          title: '品牌名称'
+        },
+        {
+          key: 'brand_url',
+          title: '品牌网址'
+        },
+        {
+          key: 'brand_logo',
+          title: '品牌网址',
+          slot: 'logo'
+        },
+        {
+          key: 'brand_des',
+          title: '品牌描述'
+        },
+        {
+          key: 'brand_status',
+          title: '品牌状态',
+          slot: 'status'
+        },
+        {
+          align: 'center',
+          title: '操作',
+          slot: 'action'
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.loadBrandList({})
+  },
+  methods: {
+    loadBrandList(data) {
+      getBrand(data).then(res => this.data = res.data)
+    },
+    // 编辑品牌
+    handleBrandEdit(row, i) {
+      // TODO 
+    },
+    // 删除品牌
+    handleBrandRemove(row) {
+      this.modalInstance('warning', '您确定要删除该品牌吗？', () => {
+        removeBrand({id: row.id}).then(res => {
+          this.$Message.success('删除成功', 1500)
+          this.loadBrandList({})
+        })
+      })
       
     }
   }
--- src/service/brand-service.js
+++ src/service/brand-service.js
@@ -1,8 +1,20 @@
-import {httpPost} from 'utils/request'
+import {httpPost, httpGet} from 'utils/request'
 const host = 'brand'
 export const addBrand = (data) => {
   return httpPost({
     url: `${host}/add`,
+    data
+  })
+}
+export const getBrand = (data) => {
+  return httpGet({
+    url: `${host}/get`,
+    data
+  })
+}
+export const removeBrand = (data) => {
+  return httpGet({
+    url: `${host}/del`,
     data
   })
 }
